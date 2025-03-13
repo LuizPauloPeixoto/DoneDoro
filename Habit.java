b30083c1adf3752308032436c386945e21675bdd
Luiz Paulo <168488341+LuizPauloPeixoto@users.noreply.github.com>
2025-03-13 17:24:58 +0000

    habit_code

---
+++ Habit.java
@@ -0,0 +1,29 @@
+public class  Habit {
+    private String name;
+    private int streak; //Quantidade de dias consecutivos
+
+
+
+
+public Habit (String name, int streak){
+
+        this.name = name;
+        this.streak = streak;
+
+    }
+
+public String getName(){
+        return name;
+
+    }
+
+public int getStreak(){
+    reurn streak;
+
+    }
+
+public void incrementStreak() {
+    this.streak++; //aumenta a sequencia em + 1 ao complear um habito
+    }
+}
+
